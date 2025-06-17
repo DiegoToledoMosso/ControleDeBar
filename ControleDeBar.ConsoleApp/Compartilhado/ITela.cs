7d216b83c101dcce1bd27e9d8291b4a07591ecaa
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-06-17 20:49:31 -0300

    Implementada Generics e Interface no ModuloMesa.

--- ControleDeBar.ConsoleApp/Compartilhado/ITela.cs
+++ ControleDeBar.ConsoleApp/Compartilhado/ITela.cs
@@ -2,5 +2,9 @@
 
 public interface ITela
 {
-
+    char ApresentarMenu();
+    void CadastrarRegistro();
+    void EditarRegistro();
+    void ExcluirRegistro();
+    void VisualizarRegistros(bool exibirCabecalho);
 }
